24251d1a60427cb8dc29d52590e881286e1899af
Ben Ward <ben.ward@softwire.com>
2020-12-25 17:45:42 +0000

    Use stdlib pow()

--- day25.py
+++ day25.py
@@ -7,18 +7,7 @@ with open("input/day25.txt") as puzzle_input:
     door_key = int(puzzle_input.readline())
 
 
-def _transform(subject, loop_size):
-    out = 1
-    subject = subject % modulus
-    while loop_size > 0:
-        if loop_size % 2 == 1:
-            out = (out * subject) % modulus
-        loop_size = loop_size >> 1
-        subject = (subject * subject) % modulus
-    return out
-
-
 for l in itertools.count(1):
-    if _transform(7, l) == card_key:
-        print(_transform(door_key, l))
+    if pow(7, l, modulus) == card_key:
+        print(pow(door_key, l, modulus))
         break
